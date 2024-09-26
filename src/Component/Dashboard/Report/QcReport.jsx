287f20bba83167ff6fde78f7dce483353e4e08d5
akshaybaisware <akshaybaisware1998@gmail.com>
2024-09-26 17:21:55 +0530

    hello css

--- src/Component/Dashboard/Report/QcReport.jsx
+++ src/Component/Dashboard/Report/QcReport.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./QcReport.css";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import { Box, Center, Flex, Input, Stack, Text } from "@chakra-ui/react";
 
 function QcReport() {
   const location = useLocation();
@@ -44,12 +45,82 @@ function QcReport() {
 
   return (
     <>
-      <div>Name : {state.name}</div>
-      <div>Mobile : {state.mobile}</div>
-      <div>Address : {state.address}</div>
-      <div>Correct form: {state?.correctform}</div>
-      <div>Incorrect form : {state?.incorrectform}</div>
-      <div>
+  <Center color={"red"} fontSize={"1.3rem"} fontWeight={"600"}>
+    
+     QC Report </Center>
+<Center mb={"1rem"}>
+<Box 
+      borderWidth="1px" 
+      borderRadius="lg" 
+      p={4} 
+      boxShadow="md" 
+      maxW="100vw" 
+      margin="auto" 
+      bg="#8bd6b2" // Light background color
+     
+    >
+      <Flex direction="row" spacing={4}>
+        {/* Name Row */}
+        <Flex
+        width={["20vw" , "25vw"]}
+        direction="column" mb={4}>
+          <Text fontWeight="bold">Name:</Text>
+          <Text>{state.name}</Text>
+        </Flex>
+
+        {/* Mobile Row */}
+        <Flex
+        
+        width={["20vw" , "15vw"]}
+        direction="column" mb={4}>
+          <Text fontWeight="bold">Mobile:</Text>
+          <Text>{state.mobile}</Text>
+        </Flex>
+
+        {/* Address Row */}
+       
+        <Flex
+         width={["20vw" , "15vw"]}
+        direction="column" mb={4}>
+          <Text fontWeight="bold">Start-Date:</Text>
+          <Text >{state.startDate.slice(0, 10)}</Text> {/* Slicing here */}
+        </Flex>
+        <Flex 
+        
+        width={["20vw" ,"10vw"]}
+        direction="column" mb={4}>
+          <Text fontWeight="bold">End-Date:</Text>
+          <Text 
+          // bg={"black"}
+       >{state.endDate.slice(0, 10)}</Text> {/* Slicing here */}
+        </Flex>
+
+        <Flex 
+         width={["20vw" , "10vw"]}
+        direction="column" mb={4}>
+          <Text fontWeight="bold">Total Form:</Text>
+          <Text>540</Text>
+        </Flex>
+
+        {/* Correct Form Row */}
+        <Flex
+         width={["20vw" , "10vw"]}
+        direction="column" mb={4}>
+          <Text fontWeight="bold">Correct Form</Text>
+          <Text>{state?.correctAssignmentCount}</Text>
+        </Flex>
+
+        {/* Incorrect Form Row */}
+        <Flex 
+          width={["20vw" , "10vw"]}
+        direction="column" mb={4}>
+          <Text fontWeight="bold">Incorrect Form</Text>
+          <Text>{state?.incorrectAssignmentCount}</Text>
+        </Flex>
+      </Flex>
+    </Box>
+    </Center>
+      {/* <div>
         {data.map((item, index) => (
           <div key={index} className="form-container">
             <div className={`form-group ${hasError("firstname", index)}`}>
@@ -86,14 +157,68 @@ function QcReport() {
               <label>Zip Code:</label>
               <input type="text" value={item.zipcode} readOnly />
             </div>
-{/* 
-            <div className={`form-group ${hasError("userId", index)}`}>
-              <label>User ID:</label>
-              <input type="text" value={item.userId} readOnly />
-            </div> */}
+           
           </div>
         ))}
-      </div>
+      </div> */}
+      <div>
+  {data.map((item, index) => (
+    <Box
+      key={index}
+      p={4}
+      bg="#f7f7f7"
+      mb={4}
+      borderRadius="md"
+      boxShadow="md"
+    >
+      <Flex justify="space-between" mb={2} flexWrap="wrap">
+        <Box flex="1" minWidth="150px" mr={2} className={hasError("firstname", index)}>
+          <Text fontWeight="bold">First Name:</Text>
+          <Input value={item.firstname} readOnly />
+        </Box>
+
+        <Box flex="1" minWidth="150px" mr={2} className={hasError("lastname", index)}>
+          <Text fontWeight="bold">Last Name:</Text>
+          <Input value={item.lastname} readOnly />
+        </Box>
+
+        <Box flex="1" minWidth="150px" className={hasError("email", index)}>
+          <Text fontWeight="bold">Email:</Text>
+          <Input value={item.email} readOnly />
+        </Box>
+      </Flex>
+
+      <Flex justify="space-between" mb={2} flexWrap="wrap">
+        <Box flex="1" minWidth="150px" mr={2} className={hasError("phonenumber", index)}>
+          <Text fontWeight="bold">Phone Number:</Text>
+          <Input value={item.phonenumber} readOnly />
+        </Box>
+
+        <Box flex="1" minWidth="150px" mr={2} className={hasError("licencenumber", index)}>
+          <Text fontWeight="bold">License Number:</Text>
+          <Input value={item.licencenumber} readOnly />
+        </Box>
+
+        <Box flex="1" minWidth="150px" className={hasError("ip", index)}>
+          <Text fontWeight="bold">IP:</Text>
+          <Input value={item.ip} readOnly />
+        </Box>
+      </Flex>
+
+      <Flex justify="space-between" mb={2} flexWrap="wrap">
+        <Box flex="1" minWidth="150px" className={hasError("zipcode", index)}>
+          <Text fontWeight="bold">Zip Code:</Text>
+          <Input value={item.zipcode} readOnly />
+        </Box>
+      </Flex>
+
+      {/* Dashed Border Line */}
+      {index < data.length - 1 && (
+        <Box as="hr" borderColor="gray.300" borderWidth="1px" borderStyle="dashed" my={4} />
+      )}
+    </Box>
+  ))}
+</div>
     </>
   );
 }
