baebb7e80a23171fcf011e01fda90d1f6ddcda20
akshaybaisware <akshaybaisware1998@gmail.com>
2025-01-30 18:55:38 +0530

    hello

--- src/Component/StampPaper/StampPaper.jsx
+++ src/Component/StampPaper/StampPaper.jsx
@@ -240,7 +240,7 @@ const StampPaper = () => {
             2.1 REMUNERATION: - - For the form filling service rendered by the
             client. The Clients shall beentitled to payments of price 43Rs.
             (INR) Max perform for the salary may vary from 22790/- INR Total
-            Form will be 530 and per form woll be 42 and the u dont have to
+            Form will be 530 and per form woll be 43 and the u dont have to
             correct more than 477 form The Client shall raise the invoice within
             5 days after the successful completion of the project. The
             contractor will pay each project's amount within a maximum of 24
